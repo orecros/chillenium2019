60396ad43ca599039b1d6d9ddb2031a51e43bda1
Bill Dang <sparkfire.ffs@gmail.com>
2019-10-05 13:58:08 -0500

    Added pause menu and main menu

--- Assets/Scripts/GlobalCanvas.cs
+++ Assets/Scripts/GlobalCanvas.cs
@@ -6,8 +6,16 @@ public class GlobalCanvas : MonoBehaviour {
 
     public static GameObject canvas;
 
+    public GameObject pauseMenu;
+
     private void Awake() {
         canvas = gameObject;
+        try {
+            if(pauseMenu == null)
+                pauseMenu = transform.Find("Pause Menu").gameObject;
+        } catch {
+            Debug.LogError("No pause menu found in canvas!");
+        }
     }
 
 }
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+    
+    public GameObject main, credits;
+
+    private void Awake() {
+        if(credits.activeSelf)
+            SwapMenu();
+    }
+
+    public void SwapMenu() {
+        if(credits.activeSelf) {
+            main.SetActive(true);
+            credits.SetActive(false);
+        } else {
+            main.SetActive(false);
+            credits.SetActive(true);
+        }
+    }
+
+    public void LoadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+}
