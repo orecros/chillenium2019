d840b3d77a445714ea3ecfc4258817f9ae741293
Bill Dang <sparkfire.ffs@gmail.com>
2019-10-05 17:55:59 -0500

    Added menu navigation with controller

--- Assets/Scripts/GlobalCanvas.cs
+++ Assets/Scripts/GlobalCanvas.cs
@@ -7,15 +7,66 @@ public class GlobalCanvas : MonoBehaviour {
     public static GameObject canvas;
 
     public GameObject pauseMenu;
+    
+    private RectTransform arrow;
+    private bool scrollBuffer;
+    private int arrowPos = 0;
 
     private void Awake() {
         canvas = gameObject;
         try {
             if(pauseMenu == null)
                 pauseMenu = transform.Find("Pause Menu").gameObject;
+            arrow = pauseMenu.transform.Find("Arrow").GetComponent<RectTransform>();
         } catch {
             Debug.LogError("No pause menu found in canvas!");
         }
+    }
+
+    private void Update() {
+        if(GameManager.Paused) {
+            if(MaxAbsVerticalInput() > 0.125f) {
+                if(!scrollBuffer) {
+                    StartCoroutine(ScrollPause());
+                    arrowPos += GetScrollDir();
+                    UpdateArrow();
+                }
+            } else
+                scrollBuffer = false;
+
+            if(Input.GetButtonDown("Act1") || Input.GetButtonDown("Act2") || Input.GetButtonDown("Act3")) {
+                if(arrowPos == 0)
+                    GameManager.Paused = false;
+                else 
+                    GameManager.LoadSceneStatic("MainMenu");
+            }
+        }
+    }
+
+    private IEnumerator ScrollPause() {
+        scrollBuffer = true;
+        yield return new WaitForSeconds(0.25f);
+        scrollBuffer = false;
+    }
+
+    private void UpdateArrow() {
+        if(arrowPos >= 2)
+            arrowPos = 0;
+        else if(arrowPos <= -1)
+            arrowPos = 1;
+
+        if(arrowPos == 0)
+            arrow.localPosition = new Vector3(-125f, -10f, 0);
+        else
+            arrow.localPosition = new Vector3(-125f, -55f, 0);
+    }
+
+    private float MaxAbsVerticalInput() {
+        return Mathf.Max(Mathf.Abs(Input.GetAxis("Vertical1")), Mathf.Abs(Input.GetAxis("Vertical2")), Mathf.Abs(Input.GetAxis("Vertical3")));
+    }
+
+    private int GetScrollDir() {
+        return (int)Mathf.Sign(Input.GetAxis("Vertical1") + Input.GetAxis("Vertical2") + Input.GetAxis("Vertical3"));
     }
 
 }
